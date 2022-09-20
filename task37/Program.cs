fb9cac1951f62f215b1de486e9dd8789bf4b9a0f
Svetikova Marina <m-golovleva@yandex.ru>
2022-09-20 18:18:30 +0500

    task37-1

--- task37/Program.cs
+++ task37/Program.cs
@@ -3,16 +3,16 @@
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
 
-int[] arr = {1, 2, 3, 4, 5};
-int[] arrnew = new int [(arr.Length+1)/2]; //(7+1)/2=4; (4+1)/2=2
-for (int i=0; i<arrnew.Length;i++)
+int[] arr = { 1, 2, 3, 4, 5 };
+int[] arrnew = new int[(arr.Length + 1) / 2]; //(7+1)/2=4; (4+1)/2=2
+for (int i = 0; i < arrnew.Length; i++)
 {
-    arrnew [i] = arr[i]*arr[arr.Length -1-i];
+    arrnew[i] = arr[i] * arr[arr.Length - 1 - i];
 }
-if(arr.Length%2!=0)
+if (arr.Length % 2 != 0)
 {
-    arrnew[arrnew.Length-1]=arr[arr.Length/2];
+    arrnew[arrnew.Length - 1] = arr[arr.Length / 2];
 }
-Console.Write(string.Join(", ",arrnew));
+Console.Write(string.Join(", ", arrnew));
 
 
