4f0d2dad3ccc2ba231a313c6f865c3b807da99b1
Svetikova Marina <m-golovleva@yandex.ru>
2022-09-20 16:00:43 +0500

    task32

---
+++ task32/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените
+//  на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] GetArray(int size, int leftRange,int rightRange)
+{
+     int[] arr = new int[size];
+     Random rand = new Random();
+     for (int i=0; i < size; i++)
+     {
+        arr[i] = rand.Next(leftRange, rightRange+1);
+     }
+     return arr;
+}
+
+int[] NumChanghe(int[] arr)
+{
+    for(int i=0; i<arr.Length; i++)
+    {
+       arr[i]=arr[i]*-1; 
+    }
+    return arr;
+}
+
+int[] array = GetArray(10,-10,10);
+Console.WriteLine(string.Join(",",array));
+int[] newArray = NumChanghe(array);
+Console.WriteLine(string.Join(",",newArray));
